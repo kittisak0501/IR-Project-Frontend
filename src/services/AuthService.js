dea8ab1c73c4520ab2fe8377ca9a76c110d0ecd0
kittisak <kittisak_kae@cmu.ac.th>
2023-01-12 16:38:37 +0700

    added user database

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,5 +1,26 @@
-import apiClient from './AxiosClient.js'
+import axios from 'axios'
+import router from '@/router'
 import GStore from '@/store'
+const apiClient = axios.create({
+  baseURL: "http://localhost:3004/users",
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+apiClient.interceptors.response.use(
+  function (response) {
+    return response
+  },
+  function (error) {
+    if (401 === error.response.status) {
+      router.push('/login')
+      return Promise.resolve(error.response)
+    } else {
+      return Promise.reject(error)
+    }
+  }
+)
 export default {
   login(user) {
     return apiClient
